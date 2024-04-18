8fec15fa5fad981c26302c511f8a9537e1f3c049
DaeYun3610 <DaeYun3610@users.noreply.github.com>
2024-04-18 17:38:05 +0900

    방명록 페이지 및  자바스크립트

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-function makeCard() {
+/*function makeCard() {
     let title = $('#title').val();
     let name = $('#name').val();
     let txt = $('#txt').val();
@@ -6,11 +6,27 @@ function makeCard() {
     let temp_html = `
 <p class="card-group">
 <div class="card border-primary mb-3" style="max-width: 18rem;">
-<div class="card-header">홍길동</div>
+<div class="card-header">${title}</div>
 <div class="card-body text-primary">
-    <h5 class="card-title">14조 화이팅</h5>
-    <p class="card-text">응원합니다.</p>
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">${txt}</p>
 </div>
 </p>`;
     $('#card').append(temp_html);
 }
+*/
+function makeCard() {
+    let title = $('#title').val();
+    let name = $('#name').val();
+    let txt = $('#txt').val();
+    alert('저장되었습니다! 감사합니다');
+    let temp_html = ` 
+    <div id = "onecard" class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">${title}</div>
+        <div class="card-body text-primary">
+            <h5 class="card-title">${name}</h5>
+            <div class="card-text">${txt}</div>
+        </div>
+    </div>
+</div>`
+$('#cardcontainer').append(temp_html);}
