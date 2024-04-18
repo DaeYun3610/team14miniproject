fd0c86900eddaab31f42b6b5c2a978e9a1294f74
DaeYun3610 <DaeYun3610@users.noreply.github.com>
2024-04-18 21:55:44 +0900

    엔터기능 추가

--- book.js
+++ book.js
@@ -29,25 +29,28 @@ function makeCard() {
         </div>
     </div>
 </div>`
-$('#cardcontainer').append(temp_html);}
-
-document.getElementById('enter').addEventListener('keydown', function(e){
-    if(e.keycode === 13 ){
-        let title = $('#title').val();
-        let name = $('#name').val();
-        let txt = $('#txt').val();
-        alert('저장되었습니다! 감사합니다');
-        let temp_html = ` 
-        <div id = "onecard" class="card border-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">${title}</div>
-            <div class="card-body text-primary">
-                <h5 class="card-title">${name}</h5>
-                <div class="card-text">${txt}</div>
-            </div>
-        </div>
-    </div>`
     $('#cardcontainer').append(temp_html);
-        
-    }
+}
 
+/*$(function(){
+    $("#txt").keydown(function(e){
+    if(e.key === 'Enter'){
+        makeCard()
+    }
+ })
+
+ })*/
+ /*$("#txt").keydown(function(e){
+    if(e.key === 'Enter'){
+        makeCard()
+    }
+})*/
+
+$(document).ready(function(){
+    $("#txt").keydown(function(e){
+        if(e.isComposing || e.keyCode === 229) return ;
+        if(e.key === 'Enter'){
+            makeCard()
+        }
+    })
 })
